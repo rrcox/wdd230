bf80f135edfeb882708403213bed60f378463f92
Rob Cox <rob_cox@binary-development.com>
2022-01-31 06:46:19 -0500

    Final version for lesson 2

--- lesson2/js/lesson2.js
+++ lesson2/js/lesson2.js
@@ -1,7 +1,7 @@
 // Set copyright year in footer
 const currentDate = new Date(document.lastModified);
 const copyrightYear = currentDate.getFullYear();
-document.querySelector("footer div span").textContent = copyrightYear;
+document.getElementById("copyright").textContent = copyrightYear;
 
 // Set last modified date in footer
 const lastModified = document.lastModified;
